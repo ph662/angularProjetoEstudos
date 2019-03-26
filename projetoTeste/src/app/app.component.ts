c5a64aaf6ad6746d6e312b8a93501579f7379183
Phelipe de Carvalho Lima <phelipe662@gmail.com>
2019-03-25 23:18:05 -0300

    1.8 - estrutura inicial - componente, decorator, webpack.

--- projetoTeste/src/app/app.component.ts
+++ projetoTeste/src/app/app.component.ts
@@ -1,5 +1,21 @@
 import { Component } from '@angular/core';
 
+
+/*
+Tudo que formos criar ao longo do curso ficará dentro de "app",
+ em que há app.component.ts
+
+Classe Ts que declara o componente, 
+o decorator @Componente possui metadados para declarar o componente
+
+selector: nome do componente que sera usado fora daqui como se fosse html (usado em index.html)
+templateUrl: link para o html
+styleUrls: link para o css
+
+
+Pra todo esse processo funcionar, o angular usar o 
+webpack(encapsulado pelo angular CLI), module bundler
+*/
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
