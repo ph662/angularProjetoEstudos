dcc2050fdaa6bf6b80a2396ca12edb12252cdab9
Phelipe de Carvalho Lima <phelipe662@gmail.com>
2019-04-03 21:35:06 -0300

    2.2 - Criando o primeiro component, com erro.

--- projetoTeste/src/app/app.component.ts
+++ projetoTeste/src/app/app.component.ts
@@ -22,7 +22,5 @@ webpack(encapsulado pelo angular CLI), module bundler
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title='Projeto do Phelipe';
-  description="Pássaro";
-  url="https://upload.wikimedia.org/wikipedia/commons/b/bd/Red_Warbler_-_Sinaloa_-_Mexico_S4E1238_%2822444216014%29.jpg";
+  
 }
---
+++ projetoTeste/src/app/photo/photo.componente.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ap-photo',
+    //template- para escrever o template aqui mesmo
+    templateUrl: 'photo.component.html'
+})
+export class PhotoComponente {
+
+    title='Projeto do Phelipe';
+    description="Pássaro";
+    url="https://upload.wikimedia.org/wikipedia/commons/b/bd/Red_Warbler_-_Sinaloa_-_Mexico_S4E1238_%2822444216014%29.jpg";
+
+}
